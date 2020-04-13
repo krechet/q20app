2a47a58c65b5d80c3b4b8b256cc65b3e5d262521
tgrdev <tgrdev>
2020-04-13 21:16:31 +0300

    updated installer

--- install.sh
+++ install.sh
@@ -2,4 +2,11 @@
 docker build -t q20app .
 docker create -it -p 8080:1339 --name q20app q20app
 docker start q20app
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo The app is serving at http://localhost:8080
+else
+  echo Failed
+fi
+
 
