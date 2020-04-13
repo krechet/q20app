e15a3e8e31b87a9e0c96ab5e43d90297608d9c05
tgrdev <tgrdev>
2020-04-13 15:32:10 +0300

    some testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignxOrGo from '../components/SignxOrGo'
-import SignIn from '../components/AppSignX'
 
 function buildRoute (path, Component) {
 
 	return { 
 		path: path, 
 		component: SignxOrGo, 
-		props : { success : Component, error : SignIn }
+		props : { success : Component }
 	}
 }
 
